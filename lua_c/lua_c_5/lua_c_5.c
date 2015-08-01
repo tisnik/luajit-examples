19d8763c926cdb6d9519c753a4e29399fadda8d8
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-08-01 23:04:30 +0200

    Added new example: lua_c_5

---
+++ lua_c/lua_c_5/lua_c_5.c
@@ -0,0 +1,52 @@
+/*
+ * Paty demonstracni priklad - zavolani funkce ze standardni
+ * knihovny Lua z jazyka C.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Zakladni a doplnkove funkce interpretu jazyka Lua */
+#include <lualib.h>
+#include <lauxlib.h>
+
+/* Hlavni funkce konzolove aplikace */
+int main(void)
+{
+    int result;
+
+    /* vytisteni hlavicky */
+    puts(LUA_RELEASE);
+    puts(LUA_COPYRIGHT);
+    puts(LUA_AUTHORS);
+    putchar('\n');
+
+    /* vytvoreni objektu, do nejz se uklada stav virtualniho stroje */
+    lua_State* L = luaL_newstate();
+    printf("State: %p\n", L);
+
+    /* nacteni vsech knihoven */
+    luaL_openlibs(L);
+
+    /* bude se volat funkce pojmenovana "print" */
+    lua_getglobal(L, "print");
+    /* ulozit parametr volane funkce na zasobnik */
+    lua_pushinteger(L, 42);
+    /* zavolani funkce "print" a predani jednoho parametru */
+    /* neocekavame pritom zadne vysledne hodnoty */
+    result = lua_pcall(L, 1, 0, 0);
+    /* kontrola volani funkce */
+    if (result) {
+        lua_error(L);
+        puts("Chyba pri volani funkce print()!");
+    }
+
+    /* odstraneni vsech objektu asociovanych se stavem "Lua" */
+    lua_close(L);
+
+    /* navratovy kod */
+    return 0;
+}
+
+/* finito */
+
