df170d040db194040f165ffbe3cac83043d24e61
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-09-01 22:08:53 +0200

    Lua debug library example #3

---
+++ debug/debug_example3/main.lua
@@ -0,0 +1,22 @@
+local tracer = require("tracer")
+require("module1")
+require("module2")
+
+tracer.enable()
+
+function f1()
+    print("Hello world!")
+end
+
+function f2()
+    f1()
+end
+
+function f3()
+    f2()
+end
+
+f3()
+module1_f3()
+module2_f3()
+
---
+++ debug/debug_example3/tracer.lua
@@ -0,0 +1,105 @@
+--
+-- Lua tracer v.3
+--
+local tracer = {
+    level=0,
+    pcall_level=0
+}
+
+
+
+--
+-- Update trace level (for a bit nicer output).
+--
+function tracer.updateTraceLevel(debugInfo, event)
+    -- decision between calling a function or returning from the function
+    if event == "call" then
+        tracer.level = tracer.level + 1
+    else
+        tracer.level = tracer.level - 1
+        if tracer.level < 0 then
+            tracer.level = 0
+        end
+    end
+end
+
+
+
+--
+-- Format information about the Lua function call.
+--
+function tracer.formatLuaCall(event, debugInfo)
+    return event .. " •" .. (debugInfo.name or "(unknown Lua)")
+           .. "•"
+           .. "  <" .. debugInfo.short_src .. ":" .. debugInfo.linedefined .. ">"
+end
+
+
+
+--
+-- Format information about C function call.
+--
+function tracer.formatCCall(event, debugInfo)
+    return event .. " •" .. (debugInfo.name or "(unknown C)")
+           .. "•"
+           .. "  [" .. debugInfo.what .. "]"
+end
+
+
+
+--
+-- Event handler
+--
+function tracer.eventHandler(event)
+    local output = ""
+
+    local debugInfo = debug.getinfo(3)
+
+    local output = tracer.level .. " → → → " .. string.rep("    ", tracer.level)
+
+    if debugInfo and debugInfo.currentline >= 0 then
+        output = output .. debugInfo.short_src, ":" .. debugInfo.currentline .. " "
+    end
+
+    local debugInfo = debug.getinfo(2)
+
+    tracer.updateTraceLevel(debugInfo, event)
+
+    -- 'what' attribute could have the following values:
+    --    "Lua"  for regular Lua functions
+    --    "C"    for C function
+    --    "main" for the main part of a Lua chunk.
+    if debugInfo.what == "main" then
+        if event == "call" then
+            output = output .. "begin " .. debugInfo.short_src
+        else
+            output = output .. "end " .. debugInfo.short_src
+        end
+    -- regular Lua function call
+    elseif debugInfo.what == "Lua" then
+        output = output .. tracer.formatLuaCall(event, debugInfo)
+    -- C (native) function call
+    elseif debugInfo.what == "C" then
+        output = output .. tracer.formatCCall(event, debugInfo)
+    -- should not happen :)
+    else
+        output = output .. " unknown!"
+    end
+    print(output)
+end
+
+
+
+--
+-- Register event handler for the "call" and "return" events.
+--
+function tracer.enable()
+    -- we want to call event handler for the following events:
+    -- "call"
+    -- "return"
+    debug.sethook(tracer.eventHandler, "cr")
+end
+
+
+return tracer
+
