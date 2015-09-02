72f59748e241482daec32b8c950f2c1cd0e658ac
Pavel Tisnovsky <ptisnovs@redhat.com>
2015-09-02 20:52:20 +0200

    Fixed reporting in debug_example4

--- debug/debug_example4/tracer.lua
+++ debug/debug_example4/tracer.lua
@@ -46,7 +46,7 @@ end
 -- Format information about the Lua function call.
 --
 function tracer.formatLuaCall(event, debugInfo)
-    return event .. " •" .. (debugInfo.name or "(unknown Lua)")
+    return " " .. event .. " •" .. (debugInfo.name or "(unknown Lua)")
            .. "•"
            .. "  <" .. debugInfo.short_src .. ":" .. debugInfo.linedefined .. ">"
 end
@@ -57,7 +57,7 @@ end
 -- Format information about C function call.
 --
 function tracer.formatCCall(event, debugInfo)
-    return event .. " •" .. (debugInfo.name or "(unknown C)")
+    return " " .. event .. " •" .. (debugInfo.name or "(unknown C)")
            .. "•"
            .. "  [" .. debugInfo.what .. "]"
 end
